58d984f01e2ba0d1df22cb0d86187eb368c0c21c
diego64 <diegoferreira1964@gmail.com>
2023-03-02 11:14:26 -0300

    Corrgindo o bug de busca e melhoradno a performace da aplicacao

--- src/pages/components/SearchForm/index.tsx
+++ src/pages/components/SearchForm/index.tsx
@@ -1,11 +1,11 @@
-import { MagnifyingGlass } from 'phosphor-react'
-import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useContext } from 'react'
+import { useForm } from 'react-hook-form'
+import { useContextSelector } from 'use-context-selector'
+
+import { MagnifyingGlass } from 'phosphor-react'
 
 import { TransactionsContext } from '../../../context/TransactionsContext'
-
 import { SearchFormContainer } from './styles'
 
 const searchFormSchema = z.object({
@@ -15,7 +15,12 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-  const { fetchTransactions } = useContext(TransactionsContext)
+  const fetchTransactions = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.fetchTransactions
+    },
+  )
 
   const {
     register,
